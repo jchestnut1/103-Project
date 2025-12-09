b607f679a00a1863d8a114aa85136705b3e26acd
Jay Chestnut <Jaychestnut320@yahoo.com>
2025-12-08 23:07:04 -0500

    Initial Commit

---
+++ 103-Project/Views/ShoppingListView.swift
@@ -0,0 +1,88 @@
+//
+//  ShoppingListView.swift
+//  103-Project
+//
+//  Created by Jay Chestnut on 12/8/25.
+//
+
+import SwiftUI
+
+struct ShoppingListView: View {
+    
+    @State private var shoppingList: [String] = ["Eggs", "Bread", "Milk"]
+    @State private var newItem: String = ""
+    
+    var body: some View {
+       
+        NavigationView{
+            ZStack{
+                Color.gray.opacity(0.3)
+                    .ignoresSafeArea()
+                
+                VStack{
+                    Text("Grocery List")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.green)
+                        .bold()
+                        .padding()
+                    
+                    List{
+                        ForEach(shoppingList, id: \.self){ item in
+                                Text(item)
+                        }
+                    }
+                    
+                    HStack{
+                        TextField("Add new Grocery item...", text: $newItem)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius:20)
+                                .stroke(Color.gray, lineWidth: 1)
+                                .opacity(0.5)
+                            )
+                            .padding()
+                        
+                        
+                        Button(action: {
+                            shoppingList.append(newItem)
+                            newItem = ""
+                        }){
+                            Image(systemName: "arrowshape.up.fill")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundStyle(Color.white)
+                                .cornerRadius(30)
+                        }
+                    } //End: HStack
+                    .padding()
+                    
+                    
+                    
+                    
+                }
+            } //End: ZStack
+            .navigationBarTitle("My Shopping App", displayMode: .inline)
+            
+            .toolbar {
+                Menu{
+                    Button("Sort A -> Z"){
+                        
+                    }
+                    
+                    Button("Reverse Order"){
+                        
+                    }
+                }
+                
+                label : {
+                    Image(systemName: "arrow.up.arrow.down.circle")
+                        .imageScale(.large)
+                }
+                .padding(.horizontal, 20)
+            }
+        }  //End: NavigationView
+    } //End: Body View
+} // END: ShoppingListView
+
+#Preview {
+    ShoppingListView()
+}
---
+++ 103-Project/_03_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _03_ProjectApp.swift
+//  103-Project
+//
+//  Created by Jay Chestnut on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct _03_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
